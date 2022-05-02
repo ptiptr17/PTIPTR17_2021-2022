2ce018f3ea3bd0c6af5c91d60d89358d70e3cd9b
ptiptr17 <104532594+ptiptr17@users.noreply.github.com>
2022-05-02 20:17:07 +0100

    Add files via upload

---
+++ project_greenmarket/php/login.php
@@ -0,0 +1,45 @@
+<?php 
+
+require_once 'openconn.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+$username = $_POST["uname"];
+$pwd = $_POST["psw"];
+$accountT = $_POST["accountType"];
+
+$queryu = mysqli_query($conn, "select * from userinfo where username='" . $username . "' and password='" . $pwd . "'");
+
+if (mysqli_num_rows($queryu) == 1) {
+    $_SESSION["loggedIn"] = true;
+    $_SESSION["username"] = $username;
+    header("location: welcome.html");
+    exit();
+}
+
+$querytrans = mysqli_query($conn, "select * from userinfo where username='" . $username . "' and password='" . $pwd . "' and accountType='" . $accountT . "'");
+print_r($querytrans);
+if (mysqli_num_rows($querytrans) == 1) {
+    $_SESSION["loggedIn"] = true;
+    $_SESSION["username"] = $username;
+    if $accountT = "Transportadora"{
+        header("location: transportadora.html");
+        exit();
+    } elseif $accountT = "Vendedor"{
+        header("location: vendedor.html");
+        exit();
+    }else{
+        header("location: user.html");
+        exit();
+    }
+
+} else {
+    header("location: login.php");
+}
+
+mysqli_close($conn);
+?>
