f231e11f709f7307dec3dcae01f2eed01f0b1dd8
franciscofdias <franciscofdias2@gmail.com>
2022-05-08 19:23:47 +0100

    atualiza

--- project_greenmarket/php/openconn.php
+++ project_greenmarket/php/openconn.php
@@ -1,22 +1,26 @@
-<!DOCTYPE html>
-<html>
-<body>
+<?php
+	echo "<p>All work and no play makes jack a dull boy!</p>";
+	$dbhost = "sql212.epizy.com";
+	$dbuser = "epiz_31612828";
+	$dbpass = "aa4ulaYtRDKpoG";
+	$dbname = "epiz_31612828_GreenMarket";
 
-	<?php
-	echo "<p>All work and no play makes jack a dull boy!</p>";*/
-	$dbhost = "sql113.epizy.com";
-	$dbuser = "epiz_31611966";
-	$dbpass = "3c1ziqiwKc1hjCV";
-	$dbname = "epiz_31611966_anewapptest";
-	
 	// Cria a ligação à BD
 	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-	
+
 	// Verifica a ligação à BD
 	if (mysqli_connect_error()) {
-	die("Database connection failed: " . mysqli_connect_error());
+        die("Database connection failed: " . mysqli_connect_error());
 	}
 	echo "<p> connection worked!</p>";
-	?>
-</body>
-</html>
+
+	public function getData(){
+        $sql = "SELECT * FROM $this->tablename";
+
+        $result = mysqli_query($this->con, $sql);
+
+        if(mysqli_num_rows($result) > 0){
+            return $result;
+        }
+    }
+?>
---
+++ project_greenmarket/php/register.php
@@ -0,0 +1,50 @@
+<?php
+include "openconn.php";
+session_start();
+
+echo $_POST['name_user'];
+$email_user = $_POST['email_user'];
+$nome_user = $_POST['name_user'];
+$pass_user = $_POST['psw_user'];
+$repass_user = $_POST['psw-repeat'];
+
+echo "as variáveis guardadas<br>";
+echo $email_user;
+echo "<br>";
+echo $nome_user;
+echo "<br>";
+echo $pass_user;
+echo "<br>";
+echo"end variáveis";
+
+if($pass_user == $repass_user){
+    echo "password correta.<br>";
+}
+
+//encriptação da password para garantir confidencialidade
+$password = password_hash($pass_repr, PASSWORD_BCRYPT);
+
+// Inserir para a tabela
+$insert_user = "insert into customer_login(username, password, email) values('$nome_user','$pass_user','$email_user')";
+
+$res2= mysqli_query ($conn, $insert_user);
+if($res2){
+    echo "Novo utilizador criado com sucesso<br>";
+    header( "refresh:30; url=../html/homepage.html");
+} else {
+    echo "Erro: insert failed" . $query . "<br>" . mysqli_error($conn);
+    header( "refresh:30; url=../html/homepage.html" );
+}
+// Termina a ligação com a base de dados
+mysqli_close($conn);
+
+// $_SESSION['username'] = $nome_repr;
+
+// Welcome message
+//$_SESSION['success'] = "You have logged in";
+
+// Page on which the user will be
+// redirected after logging in
+//header('location: home.php');
+
+?>
