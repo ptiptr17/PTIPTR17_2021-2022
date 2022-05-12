70ec95178a4d746685f1603410b99d240307cb78
fc53304 <80914551+foolemp@users.noreply.github.com>
2022-05-13 00:02:11 +0100

    correção definitiva ao login.php
    made by 53304

--- project_greenmarket/php/login.php
+++ project_greenmarket/php/login.php
@@ -3,33 +3,37 @@
 echo "inicio do processo login";
 
 /*require_once 'openconn.php';*/
-include 'openconn.php';
+include "openconn.php";
+session_start();
 
+/*
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-session_start();
+ */
 
 $username = $_POST["uname"];
 $pwd = $_POST["psw"];
 $accountT = $_POST["accountType"];
 
-echo "prints de dados do user";
+echo "prints de dados do user<br>";
+echo "username: ";
 echo $username;
+echo "password: ";
 echo $pwd;
 
-$queryu = mysqli_query($conn, "select * from customer_login where username='" . $username . "' and password='" . $pwd . "'");
+$query = "SELECT * FROM customer_login WHERE username='$username' AND password='$pwd'";
+$queryu = mysqli_query($conn, $query);
 
 if (mysqli_num_rows($queryu) == 1) {
     $_SESSION["loggedIn"] = true;
     $_SESSION["username"] = $username;
-    header("refresh:30; url= ../html/homepage.html");
-    exit()
+    echo "login efetuado com sucesso";
+    header("refresh:15; url= ../html/homepage.html");
 } else {
-    header("refresh:30; url= ../html/login.html");
+    header("refresh:15; url= ../html/login.html");
 }
 
-mysqli_close($conn);
+//mysqli_close($conn);
 
 ?>
