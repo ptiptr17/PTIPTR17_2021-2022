bb7f356b849bc3e6724ec024dd48f8dbeec88123
fc53304 <80914551+foolemp@users.noreply.github.com>
2022-05-18 22:14:36 +0100

    correcting openconn.php(53304)

--- project_greenmarket/php/openconn.php
+++ project_greenmarket/php/openconn.php
@@ -1,4 +1,5 @@
 <?php
+phpinfo();
 echo "a iniciar ligação à bd<br>";
 /*
 $dbhost = "sql212.epizy.com";
@@ -6,68 +7,83 @@ $dbuser = "epiz_31612828";
 $dbpass = "aa4ulaYtRDKpoG";
 $dbname = "34.175.213.0 ";
  */
+
 $dbhost = "localhost";
 $dbhost2 = "34.175.213.0";
 $dbname = "greenmarket_database";
 $dbuser = "ptiptr17";
 $dbpass = "ptiptr202217";
+$dbuser2= "root";
+$dbpass2= "";
 
 echo "a tentar conexão<br>";
 
 // Cria a ligação à BD
 
-//$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+/*
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+echo "erro no $conn.";
+
 // Verifica a ligação à BD
-//if (mysqli_connect_error()) {
-//      echo "connection failed<br>";
-//die("Database connection failed: " . mysqli_connect_error());
-//
-//}
-//echo "<p> connection worked!</p>";
+if (mysqli_connect_error()) {
+echo "connection failed<br>";
+die("Database connection failed: " . mysqli_connect_error());
 
-//$conn = mysqli_connect(null, $dbuser, $dbpass, $dbname, null, "ptiptr17greenmarket:europe-southwest1:greenmarketsql");
-// Verifica a ligação à BD
-//if($conn != null) {
-//      print("Connected! :*");
-//} else {
-//      print("Can't connect! :(");
-//}
+}
+echo "<p> connection worked!</p>";
 
-//try{
-//$conn = new PDO("mysql:host=".$dbhost."; dbname=". $dbname, $dbuser, $dbpass);
-//$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//}catch (PDOException $e){
-//      echo 'Database connection error:' . $e->getMessage();
-//}
-//
-//if($conn){
-//    echo "<br>connection worked.";
-//}
+ */
+
+/*
+$conn = mysqli_connect(null, $dbuser, $dbpass, $dbname, null, "ptiptr17greenmarket:europe-southwest>
+//Verifica a ligação à BD
+if($conn != null) {
+print("Connected! :*");
+} else {
+print("Can't connect! :(");
+}
+ */
+
+try{
+    $conn = new PDO("mysql:host=".$dbhost."; dbname=". $dbname, $dbuser, $dbpass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e){
+    echo 'Database connection error:' . $e->getMessage();
+}
+
+if($conn){
+    echo "<br>connection worked.";
+}
 
 
+/*
 $username = 'ptiptr17';
+$username2 = "root";
 $password = 'ptiptr202217';
+$password2 = "";
 $dbName = 'greenmarket_database';
 $connectionName = getenv("ptiptr17greenmarket:europe-southwest1:greenmarketsql");
 $socketDir = getenv('DB_SOCKET_DIR') ?: '/cloudsql';
 
 // Connect using UNIX sockets
 $dsn = sprintf(
-    'mysql:dbname=%s;unix_socket=%s/%s',
-    $dbName,
-    $socketDir,
-    $connectionName
+'mysql:dbname=%s;unix_socket=%s/%s',
+$dbName,
+$socketDir,
+$connectionName
 );
 
 // Connect to the database.
 $conn = new PDO($dsn, $username, $password, $conn_config);
 
 if($conn != null) {
-        print("Connected! :*");
+print("Connected! :*");
 } else {
-        print("Can't connect! :(");
+print("Can't connect! :(");
 }
-
+ */
 
 /*
 public function getData(){
