bbd6749ecc4318195d360ba77039bd1e8aaa3348
franciscofdias <franciscofdias2@gmail.com>
2022-06-16 18:25:38 +0100

    registo de armazém quase completo

---
+++ project_greenmarket/php/registerWarehouse.php
@@ -0,0 +1,51 @@
+<?php
+include "openconn.php";
+session_start();
+
+echo $_POST['name_warehouse'];
+$phone_warehouse = $_POST['phone_warehouse'];
+$nome_warehouse = $_POST['name_warehouse'];
+$province_warehouse = $_POST['province_warehouse'];
+$city_warehouse = $_POST['city_warehouse'];
+$district_warehouse = $_POST['district_warehouse'];
+$address_warehouse = $_POST['address_warehouse'];
+
+echo "as vari�veis guardadas<br>";
+echo $phone_warehouse;
+echo "<br>";
+echo $nome_warehouse;
+echo "<br>";
+echo $province_warehouse;
+echo "<br>";
+echo $city_warehouse;
+echo "<br>";
+echo $district_warehouse;
+echo "<br>";
+echo $address_warehouse;
+echo "<br>";
+echo"end vari�veis";
+
+// Inserir para a tabela
+$insert_warehouse = "insert into warehouse_info(warehouse_name, contact, province, city, district, address) values('$nome_warehouse','$phone_warehouse','$province_warehouse', '$city_warehouse', '$district_warehouse', '$address_warehouse')";
+
+$res2= mysqli_query ($conn, $insert_warehouse);
+if($res2){
+    echo "Novo armazém criado com sucesso<br>";
+    header( "refresh:10; url= ../html/homepage.html");
+} else {
+    echo "Erro: insert failed" . $query . "<br>" . mysqli_error($conn);
+    header( "refresh:10; url=../html/registerWarehouse.html" );
+}
+// Termina a liga��o com a base de dados
+mysqli_close($conn);
+
+// $_SESSION['username'] = $nome_repr;
+
+// Welcome message
+//$_SESSION['success'] = "You have logged in";
+
+// Page on which the user will be
+// redirected after logging in
+//header('location: home.php');
+
+?>
